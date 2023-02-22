c8e2554198c8ca00905a10aebbcf30fd4b1d9526
sirpuffin1 <angelcruz981@yahoo.com>
2023-02-22 12:46:11 -0800

    feat(index): create/import heatmap component

---
+++ components/Heatmap.tsx
@@ -0,0 +1,9 @@
+const Heatmap = () => {
+    return (
+        <div>
+            Enter
+        </div>
+    );
+}
+
+export default Heatmap;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 
 
 import styles from '@/styles/Home.module.css'
+import Heatmap from '@/components/Heatmap'
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <h1>{new Date().toISOString().replace(/T.*/, "")}</h1>
+        <Heatmap/>
       </main>
     </>
   )
